0f0a5383f418ff3b4404c274467842ebf82c9242
Rocard Fonji <fonkengrocard@gmail.com>
2024-05-09 20:24:41 +0100

    implememnt getTodo method and add error handling in mongo.ts

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -4,39 +4,72 @@ import { ITodo, TodoSchema } from '../models/todo';
 const uri = process.env.MONGO_URI ?? '';
 
 async function connectDB() {
-  try {
-    await mongoose.connect(uri);
-    console.log('MongoDB connected with Mongoose');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    try {
+      await mongoose.connect(uri);
+      console.log('MongoDB connected with Mongoose');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
   }
-}
 
 async function getTodos() {
-  const todos = await TodoSchema.find();
-  return todos;
+    try {
+        const todos = await TodoSchema.find();
+        return todos;
+    } catch (error) {
+        console.error('Error fetching todos:', error);
+        throw error;
+    }
+}
+
+async function getTodo(id: string) {
+    try {
+        const todo = await TodoSchema.findById(id);
+        if (!todo) {
+            throw new Error('Todo not found');
+        }
+        return todo;
+    } catch (error) {
+        console.error('Error fetching todo:', error);
+        throw error;
+    }
 }
 
 async function addTodo(todo: ITodo) {
-  const newTodo = new TodoSchema(todo);
-  await newTodo.save();
-  return newTodo;
+    try {
+        const newTodo = new TodoSchema(todo);
+        await newTodo.save();
+        return newTodo;
+    } catch (error) {
+        console.error('Error creating todo:', error);
+        throw error;
+    }
 }
 
 async function updateTodo(id: string, updateData: Partial<ITodo>) {
-  const updatedTodo = await TodoSchema.findByIdAndUpdate(id, updateData);
-  if (!updatedTodo) {
-    throw new Error('Todo not found');
-  }
-  return updatedTodo;
+    try {
+        const updatedTodo = await TodoSchema.findByIdAndUpdate(id, updateData, { new: true });
+        if (!updatedTodo) {
+            throw new Error('Todo not found');
+        }
+        return updatedTodo;
+    } catch (error) {
+        console.error('Error updating todo:', error);
+        throw error;
+    }
 }
 
 async function deleteTodo(id: string) {
-  const deletedTodo = await TodoSchema.findByIdAndDelete(id);
-  if (!deletedTodo) {
-    throw new Error('Todo not found');
-  }
-  return 'Todo deleted successfully';
+    try {
+        const deletedTodo = await TodoSchema.findByIdAndDelete(id);
+        if (!deletedTodo) {
+            throw new Error('Todo not found');
+        }
+        return 'Todo deleted successfully';
+    } catch (error) {
+        console.error('Error deleting todo:', error);
+        throw error;
+    }
 }
 
-export { connectDB, getTodos, addTodo, updateTodo, deleteTodo };
+export { connectDB, getTodos, getTodo, addTodo, updateTodo, deleteTodo };
