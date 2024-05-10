1c42945fd1a0c4bf5a49d17060b82d53d119170c
Rocard Fonji <fonkengrocard@gmail.com>
2024-05-10 17:35:55 +0100

    - add profiles for development and production - modify index.ts to handle both dev and prod mode - add configs for serverless deployment

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
+import { ApolloServer as DevServer } from "apollo-server";
+import { ApolloServer as ProdServer } from 'apollo-server-lambda';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import resolvers from "./graphql/resolvers";
 import typeDefs from "./graphql/schema";
+import dotenv from 'dotenv';
 
-const { ApolloServer } = require('apollo-server');
+dotenv.config();
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
+const isProduction = process.env.NODE_ENV === 'production';
 
-server.listen().then(({ url }: { url: string }) => {
+if (!isProduction) {
+    const server = new DevServer({
+        typeDefs,
+        resolvers,
+    });
 
-    console.log(`Server ready at ${url}`);
-});
+    server.listen().then(({ url }: { url: string }) => {
+        console.log(`Server ready at ${url}`);
+    });
+} else {
+    const server = new ProdServer({
+        typeDefs,
+        resolvers,
+        introspection: true,
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    });
+
+    exports.handler = server.createHandler();
+}
+
+
